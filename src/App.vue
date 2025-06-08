e458fad7dfe9677c4340160e333d7097e2b46cd9
angelovich5 <angelovichd7@gmail.com>
2025-06-08 09:27:50 -0400

    Primer commit: subiendo mi proyecto

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-app>
+
+        <v-toolbar app>
+            <v-img><img alt="Travel Logo" src="@/assets/travelconnection3.png"/></v-img>
+            <v-spacer></v-spacer>
+
+            <v-btn>Iniciar Sesión</v-btn>
+            <v-btn>Crear Cuenta</v-btn>
+        </v-toolbar>
+
+    </v-app>
+</template>
+
+<style scoped> 
+
+</style>
+
+<script setup>
+ import {ref} from 'vue'
+</script>
